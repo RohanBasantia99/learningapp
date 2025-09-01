7c9cb6f9e14336a8d46f89e442cd1eb8fe2f261b
Rohan Basantia <rohan.basantia1999@gmail.com>
2025-09-01 19:59:32 +0530

    second commit

--- obj/Debug/net8.0/learningapp.AssemblyInfo.cs
+++ obj/Debug/net8.0/learningapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("learningapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd8b776ef2920ba902b904d71e0fe3ddfa45e082")]
 [assembly: System.Reflection.AssemblyProductAttribute("learningapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("learningapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
